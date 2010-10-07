1f9aabc50d659b830dee27b86541a920d06395f5
Sean Hayes <sean@seanhayes.name>
2010-10-07 02:25:37 -0400

    added test/ directory

---
+++ test/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+#NOTE: you will have to checkout Django from SVN and symlink /django/trunk/tests/* here
+#ln -s path/to/django/trunk/tests/*
+import runtests
+
+
+
+#ControlDjangoTestCase
+
+
+#ExpDjangoTestCase
+
+#TODO: compare execution times of tests both with and without this project enabled
